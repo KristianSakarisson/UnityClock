cc502b44bfabeb4483e5d8e18f125828ed5f0caf
Kristian Sakarisson <kristian@sakarisson.com>
2016-05-19 16:50:10 +0100

    Updated script to make the clock tick once a second.

--- Assets/Scripts/ClockController.cs
+++ Assets/Scripts/ClockController.cs
@@ -12,6 +12,9 @@ public class ClockController : MonoBehaviour {
 
     Vector3 direction = new Vector3(0f, 0f, -1f);
 
+    float secondPassed = 0f;
+    float minutePassed = 0f;
+
     void Awake ()
     {
         Application.runInBackground = true;
@@ -24,17 +27,33 @@ public class ClockController : MonoBehaviour {
 
         DateTime now = System.DateTime.Now;
 
+        minutePassed = now.Second;
+
         //Calibrate current time
-        hourPointer.transform.RotateAround(middle, direction, (360f / 12f) * now.Hour + (360f / 60f / 12f) * now.Minute + (360f / 60f / 60f / 12f) * now.Second);
-        minutePointer.transform.RotateAround(middle, direction, (360f / 60f) * now.Minute + (360f / 60f / 60f / 12f) * now.Second);
-        secondPointer.transform.RotateAround(middle, direction, (360f / 60) * now.Second);
+        secondPointer.transform.RotateAround(middle, direction, (now.Second / 60f * 360f));
+
+        minutePointer.transform.RotateAround(middle, direction, now.Minute / 60f * 360f);
+
+        hourPointer.transform.RotateAround(middle, direction, now.Hour / 12f * 360f + now.Minute / (12f * 60f) * 360f);
+
     }
 	
-	void Update ()
+	void FixedUpdate ()
     {
-        //Update time
-        hourPointer.transform.RotateAround(middle, direction, (Time.deltaTime / (60 * 60 * 60)) * 360);
-        minutePointer.transform.RotateAround(middle, direction, (Time.deltaTime / (60 * 60)) * 360);
-        secondPointer.transform.RotateAround(middle, direction, Time.deltaTime / 60 * 360);
+        secondPassed += Time.deltaTime;
+        minutePassed += Time.deltaTime;
+
+        if (secondPassed >= 1)
+        {
+            secondPointer.transform.RotateAround(middle, direction, 1f / 60f * 360f);
+            secondPassed %= 1;
+        }
+        //Rotate minute and hour pointers only when a full minute has passed
+        if (minutePassed >= 60)
+        {
+            minutePointer.transform.RotateAround(middle, direction, 1f / 60 * 360);
+            hourPointer.transform.RotateAround(middle, direction, 1f / (60 * 12) * 360);
+            minutePassed %= 60;
+        }
     }
 }
